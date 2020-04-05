1653dd42e016b57e7f6b3185ff552958a61adaa8
EC2 Default User <ec2-user@ip-172-31-36-72.ap-northeast-1.compute.internal>
2020-04-05 14:47:19 +0000

    [add]second test

--- public/app.js
+++ public/app.js
@@ -1,7 +1,17 @@
 'use strict';
 var learnjs = {};
 
+learnjs.problemView = function() {
+    return $('<div class="problem-view">').text('Coming soon!');
+}
+
 learnjs.showView = function(hash) {
-    var problemView = $('<div class="problem-view">').text('Coming soon!');
-    $('.view-container').empty().append(problemView);
+    const routes = {
+        '#problem': learnjs.problemView
+    };
+    const hashParts = hash.split('-');
+    const viewFn = routes[hashParts[0]];
+    if (viewFn) {
+        $('.view-container').empty().append(viewFn(hashParts[1]));
+    }
 }
