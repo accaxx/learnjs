c3963352b407664db68b99abc612772d1c3a000e
acca <a.asahara22@gmail.com>
2020-04-17 22:32:12 +0900

    [add]1-2chapter

---
+++ public/app.js
@@ -0,0 +1,22 @@
+'use strict';
+var learnjs = {};
+
+learnjs.problemView = function(problemNumber) {
+    let title = 'Problem #' + problemNumber + ' Coming soon!';
+    return $('<div class="problem-view">').text(title);
+}
+
+learnjs.showView = function(hash) {
+    const routes = {
+        '#problem': learnjs.problemView
+    };
+    const hashParts = hash.split('-');
+    const viewFn = routes[hashParts[0]];
+    if (viewFn) {
+        $('.view-container').empty().append(viewFn(hashParts[1]));
+    }
+}
+
+learnjs.appOnReady = function() {
+    learnjs.showView(window.location.hash);
+}
